65627bab9c5b0df36ac5aed5c218badeeae5d509
smansani <94820506+smansani@users.noreply.github.com>
2024-08-27 13:46:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config(); // For loading environment variables
+
 
 const app = express();
 const port = 5000;
 
-app.use(cors()); // Allow requests from other origins
-app.use(express.json()); // Parse JSON bodies
+app.use(cors()); 
+app.use(express.json()); 
 
 const API_BASE_URL = 'http://20.244.56.144/test/companies';
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzI0NzQ2NDM5LCJpYXQiOjE3MjQ3NDYxMzksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjI5NzAyZmNiLThhMTgtNDc0NS05Yzk1LWU4NWJmNDc1NjM5OCIsInN1YiI6InNtYW5zYW5pQGdpdGFtLmluIn0sImNvbXBhbnlOYW1lIjoiZ29tYXJ0IiwiY2xpZW50SUQiOiIyOTcwMmZjYi04YTE4LTQ3NDUtOWM5NS1lODViZjQ3NTYzOTgiLCJjbGllbnRTZWNyZXQiOiJLbWNzVHFUdFFTY3BPTWZhIiwib3duZXJOYW1lIjoiTS5TcmVlaGFyc2hhIiwib3duZXJFbWFpbCI6InNtYW5zYW5pQGdpdGFtLmluIiwicm9sbE5vIjoiVlUyMUNTRU4wMTAwMjE2In0.0n6PVVHkhr0kDZtTGIB5oqYdJgHhsMbgNnZuO_k9tro"
